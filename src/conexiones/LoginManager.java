2285051175192aebc113433d622a58ecb7ccf036
Miguel1422 <cacahuate_10@outlook.com>
2017-05-05 10:23:57 -0500

    Small

--- src/conexiones/LoginManager.java
+++ src/conexiones/LoginManager.java
@@ -5,19 +5,22 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/***
+/**
+ * *
  *
- * @author Miguel
- * Clase para poder loguearse
+ * @author Miguel Clase para poder loguearse
  */
 public class LoginManager {
-    /***
+
+    /**
+     * *
      * Metodo que devuelve verdadero si el usario y contraseña son corrector
+     *
      * @param user Usuario
      * @param pass Contraseña
      * @return verdadero si existe de lo contrario falso
      */
-    public static boolean login(String user, String pass) {
+    public static int login(String user, String pass) {
         try {
             Connection micon = Conexion.getInstance().getConnection();
             String cadsql = String.format("SELECT * FROM usuarios WHERE nombre = '%s'", user);
@@ -27,15 +30,16 @@ public class LoginManager {
                 if (rs.getString("password").equals(pass)) {
                     pstm.execute();
                     pstm.close();
-                    return true;
+                    return rs.getInt("tipo");
+
                 }
             }
             pstm.execute();
             pstm.close();
-            return false;
+            return 0;
         } catch (SQLException e) {
             System.out.println("Error " + e.getMessage());
-            return false;
+            return 0;
         }
     }
 }
---
+++ src/config/Constants.java
@@ -0,0 +1,11 @@
+package config;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Constants {
+
+    public static final int ADMIN = 1;
+    public static final int USER = 2;
+}
